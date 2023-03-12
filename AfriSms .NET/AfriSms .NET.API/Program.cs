0dab0ccae99c542d508c9554e1ed045d79d4feba
NTWALI Terance <44357498+terancetigersoft@users.noreply.github.com>
2023-03-12 16:05:28 +0200

    update packages

--- AfriSms .NET/AfriSms .NET.API/Program.cs
+++ AfriSms .NET/AfriSms .NET.API/Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo() 
+    { 
+        Title = "Afri SMS", 
+        Description = "Afri SMS is a sms gateway that helps you to send bulk sms!", 
+        Version = "v1", 
+        Contact = new OpenApiContact() 
+        { 
+            Email = "afrinnovat@gmail.com", 
+            Name = "Afrinnovat Ltd", 
+            Url = new("https://www.afrinnovat.com") 
+        } 
+    });
+});
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapGet("/Welcome", () => "Welcome to Afri Sms developed by Afrinnovat Ltd!");
+app.MapPost("/SendSms", () =>
+{
 
-app.MapGet("/", () => "Hello World!");
-
+});
 app.Run();
